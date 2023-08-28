e060f71b37f3e78c0a30a811b7bdece469f95d93
ff6347 <hash@ff6347.email>
2023-08-28 13:05:37 +0200

    fix(timeouts): Move to Service role key
    Allows long running requests

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,11 +3,16 @@ import { Database } from "./database.js";
 import { EnvError } from "./errors.js";
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 if (SUPABASE_URL === undefined) throw new EnvError("SUPABASE_URL is undefined");
-if (SUPABASE_ANON_KEY === undefined)
-	throw new EnvError("SUPABASE_ANON_KEY is undefined");
-const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
-	auth: { persistSession: false },
-});
+if (SUPABASE_SERVICE_ROLE_KEY === undefined)
+	throw new EnvError("SUPABASE_SERVICE_ROLE_KEY is undefined");
+
+const supabase = createClient<Database>(
+	SUPABASE_URL,
+	SUPABASE_SERVICE_ROLE_KEY,
+	{
+		auth: { persistSession: false },
+	},
+);
 export default supabase;
