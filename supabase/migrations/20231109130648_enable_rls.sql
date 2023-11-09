e26a42190825363b5519b044b6fb3b0f2d53ff5a
ff6347 <hash@ff6347.email>
2023-11-09 14:07:50 +0100

    fix(RLS): Enable RLS on all tables
    There is no need for public access. All requests are handled by our API

---
+++ supabase/migrations/20231109130648_enable_rls.sql
@@ -0,0 +1,9 @@
+alter table "public"."processed_document_chunks" enable row level security;
+
+alter table "public"."processed_document_summaries" enable row level security;
+
+alter table "public"."processed_documents" enable row level security;
+
+alter table "public"."registered_documents" enable row level security;
+
+
