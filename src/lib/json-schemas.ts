8f86e6c44bf0988c77e0f920ab06396889531139
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-11-28 13:23:49 +0100

    fix: pass match_threshold to db

--- src/lib/json-schemas.ts
+++ src/lib/json-schemas.ts
@@ -12,7 +12,7 @@ export const documentSearchBodySchema = S.object()
 	.prop("match_threshold", S.number().minimum(0).maximum(1).default(0.85))
 	.prop("num_probes", S.number().minimum(1).maximum(49).default(7))
 	.prop("chunk_limit", S.number().minimum(1).maximum(128).default(64))
-	.prop("summary_limit", S.number().minimum(1).maximum(32).default(16))
+	.prop("summary_limit", S.number().minimum(1).maximum(64).default(16))
 	.prop("document_limit", S.number().minimum(1).maximum(10).default(3))
 	.prop("search_algorithm", S.string().default("chunks-and-summaries"))
 	.required(["query"]);
--- src/lib/similarity-search-chunks-and-summaries.ts
+++ src/lib/similarity-search-chunks-and-summaries.ts
@@ -18,10 +18,7 @@ export async function similaritySearchOnChunksAndSummaries(
 		await supabase
 			.rpc("match_summaries_and_chunks", {
 				embedding: config.embedding,
-				// ok, this is weird. the higher we set match_threshold, the longer the DB function takes to complete
-				// workaround is to set match_threshold to 0 and do the tresholding in frontend
-				// no difference in results, since the results are ordered in DB already
-				match_threshold: 0,
+				match_threshold: config.match_threshold,
 				chunk_limit: config.chunk_limit,
 				summary_limit: config.summary_limit,
 				num_probes: config.num_probes,
