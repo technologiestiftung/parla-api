74175287309c0cb3452490d13b2cbf4835ca24d9
ff6347 <hash@ff6347.email>
2023-11-30 17:45:44 +0100

    fix(migrations): Drop function before creation
    Not dropping the functions created errors while reseting the database

--- supabase/migrations/20231110134710_fix_summary_matches.sql
+++ supabase/migrations/20231110134710_fix_summary_matches.sql
@@ -1,10 +1,16 @@
-set check_function_bodies = off;
+SET check_function_bodies = OFF;
+
+DROP FUNCTION IF EXISTS public.match_summaries(embedding vector, match_threshold double precision, match_count integer, min_content_length integer, num_probes integer);
 
 CREATE OR REPLACE FUNCTION public.match_summaries(embedding vector, match_threshold double precision, match_count integer, min_content_length integer, num_probes integer)
- RETURNS TABLE(id integer, processed_document_id integer, summary text, similarity double precision)
- LANGUAGE plpgsql
-AS $function$
-	#variable_conflict use_variable
+	RETURNS TABLE(
+		id integer,
+		processed_document_id integer,
+		summary text,
+		similarity double precision)
+	LANGUAGE plpgsql
+	AS $function$
+	# variable_conflict use_variable
 BEGIN
 	EXECUTE format('SET LOCAL ivfflat.probes = %s', num_probes);
 	RETURN query
@@ -12,21 +18,21 @@ BEGIN
 		processed_document_summaries.id,
 		processed_document_summaries.processed_document_id,
 		processed_document_summaries.summary,
-		(processed_document_summaries.summary_embedding <#> embedding) * -1 as similarity
-		FROM
-			processed_document_summaries
-			-- We only care about sections that have a useful amount of content
-		WHERE
-			length(processed_document_summaries.summary) >= min_content_length
-			-- The dot product is negative because of a Postgres limitation, so we negate it
-			and(processed_document_summaries.summary_embedding <#> embedding) * -1 > match_threshold
-				-- OpenAI embeddings are normalized to length 1, so
-				-- cosine similarity and dot product will produce the same results.
-				-- Using dot product which can be computed slightly faster.
-				--
-				-- For the different syntaxes, see https://github.com/pgvector/pgvector
-			ORDER BY
-				processed_document_summaries.summary_embedding <#> embedding
-			LIMIT match_count;
+(processed_document_summaries.summary_embedding < #> embedding) * -1 AS similarity
+	FROM
+		processed_document_summaries
+		-- We only care about sections that have a useful amount of content
+	WHERE
+		length(processed_document_summaries.summary) >= min_content_length
+		-- The dot product is negative because of a Postgres limitation, so we negate it
+		AND(processed_document_summaries.summary_embedding < #> embedding) * -1 > match_threshold
+		-- OpenAI embeddings are normalized to length 1, so
+		-- cosine similarity and dot product will produce the same results.
+		-- Using dot product which can be computed slightly faster.
+		--
+		-- For the different syntaxes, see https://github.com/pgvector/pgvector
+	ORDER BY
+		processed_document_summaries.summary_embedding < #> embedding
+	LIMIT match_count;
 END;
 $function$
--- supabase/migrations/20231115161232_match_summaries_and_chunks_function.sql
+++ supabase/migrations/20231115161232_match_summaries_and_chunks_function.sql
@@ -1,47 +1,74 @@
+SET check_function_bodies = OFF;
+
+DROP FUNCTION IF EXISTS public.match_summaries_and_chunks(embedding vector, match_threshold double precision, chunk_limit integer, summary_limit integer, num_probes integer);
+
 CREATE OR REPLACE FUNCTION public.match_summaries_and_chunks(embedding vector, match_threshold double precision, chunk_limit integer, summary_limit integer, num_probes integer)
- RETURNS TABLE(processed_document_id integer, chunk_ids integer[], chunk_similarities double precision[], avg_chunk_similarity double precision, summary_ids integer[], summary_similarity double precision, similarity double precision)
- LANGUAGE plpgsql
-AS $function$
-	#variable_conflict use_variable
+	RETURNS TABLE(
+		processed_document_id integer,
+		chunk_ids integer[],
+		chunk_similarities double precision[],
+		avg_chunk_similarity double precision,
+		summary_ids integer[],
+		summary_similarity double precision,
+		similarity double precision)
+	LANGUAGE plpgsql
+	AS $function$
+	# variable_conflict use_variable
 BEGIN
-	RETURN query
-	
-	WITH chunk_winners AS (
-	    SELECT
-	        cw.id AS chunk_id,
-	        NULL AS summary_id,
-	        cw.processed_document_id,
-	        cw.similarity
-	    FROM match_document_chunks(embedding, match_threshold, chunk_limit, 0, num_probes) as cw
-	), summary_winners AS (
-	    SELECT
-	        NULL AS chunk_id,
-	        sw.id AS summary_id,
-	        sw.processed_document_id,
-	        sw.similarity
-	    FROM match_summaries(embedding, match_threshold, summary_limit, 0, num_probes) as sw
-	), all_winners AS (
-		SELECT chunk_winners.chunk_id, NULL as summary_id, chunk_winners.processed_document_id, chunk_winners.similarity FROM chunk_winners
-		UNION ALL
-	    SELECT NULL as chunk_id, summary_winners.summary_id, summary_winners.processed_document_id, summary_winners.similarity FROM summary_winners
-	)
-	
+	RETURN query WITH chunk_winners AS(
+		SELECT
+			cw.id AS chunk_id,
+			NULL AS summary_id,
+			cw.processed_document_id,
+			cw.similarity
+		FROM
+			match_document_chunks(embedding, match_threshold, chunk_limit, 0, num_probes) AS cw
+),
+summary_winners AS(
 	SELECT
-	    winners.processed_document_id,
-	    ARRAY_AGG(winners.chunk_id) FILTER (WHERE winners.chunk_id IS NOT NULL) AS chunk_ids,
-	    ARRAY_AGG(winners.similarity) FILTER (WHERE winners.chunk_id IS NOT NULL) AS chunk_similarities,
-	    AVG(winners.similarity) FILTER (WHERE winners.chunk_id IS NOT NULL) AS avg_chunk_similarity,
-	    ARRAY_AGG(winners.summary_id) FILTER (WHERE winners.summary_id IS NOT NULL) AS summary_ids,
-	    AVG(winners.similarity) FILTER (WHERE winners.summary_id IS NOT NULL) AS summary_similarity,
-	    CASE WHEN ARRAY_LENGTH(ARRAY_AGG(winners.chunk_id) FILTER (WHERE winners.chunk_id IS NOT NULL), 1) IS NULL THEN coalesce(AVG(winners.similarity) FILTER (WHERE winners.summary_id IS NOT NULL), 0)
-		WHEN ARRAY_LENGTH(ARRAY_AGG(winners.summary_id) FILTER (WHERE winners.summary_id IS NOT NULL), 1) IS NULL THEN coalesce(AVG(winners.similarity) FILTER (WHERE winners.chunk_id IS NOT NULL), 0)
-		ELSE (coalesce(AVG(winners.similarity) FILTER (WHERE winners.chunk_id IS NOT NULL), 0) + coalesce(AVG(winners.similarity) FILTER (WHERE winners.summary_id IS NOT NULL), 0)) / 2.0
+		NULL AS chunk_id,
+		sw.id AS summary_id,
+		sw.processed_document_id,
+		sw.similarity
+	FROM
+		match_summaries(embedding, match_threshold, summary_limit, 0, num_probes) AS sw
+),
+all_winners AS(
+	SELECT
+		chunk_winners.chunk_id,
+		NULL AS summary_id,
+		chunk_winners.processed_document_id,
+		chunk_winners.similarity
+	FROM
+		chunk_winners
+	UNION ALL
+	SELECT
+		NULL AS chunk_id,
+		summary_winners.summary_id,
+		summary_winners.processed_document_id,
+		summary_winners.similarity
+	FROM
+		summary_winners
+)
+SELECT
+	winners.processed_document_id,
+	ARRAY_AGG(winners.chunk_id) FILTER(WHERE winners.chunk_id IS NOT NULL) AS chunk_ids,
+	ARRAY_AGG(winners.similarity) FILTER(WHERE winners.chunk_id IS NOT NULL) AS chunk_similarities,
+	AVG(winners.similarity) FILTER(WHERE winners.chunk_id IS NOT NULL) AS avg_chunk_similarity,
+	ARRAY_AGG(winners.summary_id) FILTER(WHERE winners.summary_id IS NOT NULL) AS summary_ids,
+	AVG(winners.similarity) FILTER(WHERE winners.summary_id IS NOT NULL) AS summary_similarity,
+	CASE WHEN ARRAY_LENGTH(ARRAY_AGG(winners.chunk_id) FILTER(WHERE winners.chunk_id IS NOT NULL), 1) IS NULL THEN
+		coalesce(AVG(winners.similarity) FILTER(WHERE winners.summary_id IS NOT NULL), 0)
+	WHEN ARRAY_LENGTH(ARRAY_AGG(winners.summary_id) FILTER(WHERE winners.summary_id IS NOT NULL), 1) IS NULL THEN
+		coalesce(AVG(winners.similarity) FILTER(WHERE winners.chunk_id IS NOT NULL), 0)
+	ELSE
+(coalesce(AVG(winners.similarity) FILTER(WHERE winners.chunk_id IS NOT NULL), 0) + coalesce(AVG(winners.similarity) FILTER(WHERE winners.summary_id IS NOT NULL), 0)) / 2.0
 	END similarity
-
-	FROM all_winners AS winners
-	GROUP BY winners.processed_document_id
-	ORDER BY similarity DESC;
-
-
+FROM
+	all_winners AS winners
+GROUP BY
+	winners.processed_document_id
+ORDER BY
+	similarity DESC;
 END;
 $function$
