240a97d0a534cd5f1f90437cf3b5f6052c31759e
ff6347 <hash@ff6347.email>
2023-12-02 18:04:21 +0100

    test: config for ava

--- ava.config.mjs
+++ ava.config.mjs
@@ -3,6 +3,7 @@ config({ path: "./.env.test" });
 
 export default {
 	require: [],
+	timeout: "20s",
 	files: ["src/tests/**/*", "!src/tests/api_test.ts", "!src/tests/util/**/*"],
 	"no-worker-threads": true,
 	typescript: {
