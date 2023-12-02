9f1448df3ebee9aa7a55650f0bfb0ee08c4d6fd2
ff6347 <hash@ff6347.email>
2023-12-02 17:59:55 +0100

    test: generate answer route

--- src/tests/generate-answer.test.ts
+++ src/tests/generate-answer.test.ts
@@ -1,8 +1,57 @@
-// import nock from "nock";
+import anyTest, { TestFn } from "ava";
+import { buildTestServer } from "./util/test-server.js";
+import { FastifyInstance, InjectOptions } from "fastify";
+import { testGenAnswerReqBody } from "./util/fixtures.js";
+import { mockServer } from "../mock/node.js";
 
-// const scope = nock("https://api.openai.com")
-// 	.get("v1/chat/completions")
-// 	.reply(200, {
-// 		answer
+const test = anyTest as TestFn<{ server: FastifyInstance }>;
 
-// 	});
+test.before(async (t) => {
+	mockServer.listen();
+	const server = await buildTestServer();
+	t.context = { server };
+});
+
+test.afterEach(async () => {
+	mockServer.resetHandlers();
+});
+test.after(async (t) => {
+	await t.context.server.close();
+	mockServer.close();
+});
+
+test("generate answer route should respond with 201", async (t) => {
+	const opts: InjectOptions = {
+		method: "POST",
+		url: {
+			pathname: "/generate-answer",
+			hostname: "localhost",
+			port: 8888,
+			protocol: "http",
+		},
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(testGenAnswerReqBody),
+	};
+	const response = await t.context.server.inject(opts);
+	t.is(response.statusCode, 201);
+});
+test("generate answer route should respond with 500", async (t) => {
+	const opts: InjectOptions = {
+		method: "POST",
+		url: {
+			pathname: "/generate-answer",
+			hostname: "localhost",
+			port: 8888,
+			protocol: "http",
+		},
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ ...testGenAnswerReqBody, query: "" }),
+	};
+	const response = await t.context.server.inject(opts);
+	t.is(response.statusCode, 500);
+	t.snapshot(JSON.parse(response.body));
+});
