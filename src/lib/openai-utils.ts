6f4a5142c323ccbfded6e3f97f11adcf7bcec49c
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-08 11:30:45 +0100

    chore: update test snapshot

--- src/lib/openai-utils.ts
+++ src/lib/openai-utils.ts
@@ -33,7 +33,6 @@ export async function OpenAIStream(
 					try {
 						const json = JSON.parse(data);
 						const text = json.choices[0].delta?.content ?? "";
-						console.log(text);
 						if (counter < 2 && (text.match(/\n/) || []).length) {
 							// this is a prefix character (i.e., "\n\n"), do nothing
 							return;
