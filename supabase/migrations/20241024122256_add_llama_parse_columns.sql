eb55cc16c72bb1caf7defb75b6d4782d0c5713ba
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-10-24 14:27:36 +0200

    fix: typo

--- supabase/migrations/20241024122256_add_llama_parse_columns.sql
+++ supabase/migrations/20241024122256_add_llama_parse_columns.sql
@@ -1,5 +1,5 @@
-alter table processed_documents_summaries add column summary_llama_parse text;
-alter table processed_documents_summaries add column summary_embedding_llama_parse embedding vector(1536);
+alter table processed_document_summaries add column summary_llama_parse text;
+alter table processed_document_summaries add column summary_embedding_llama_parse embedding vector(1536);
 
-alter table processed_documents_chunks add column content_llama_parse text;
-alter table processed_documents_chunks add column embedding_llama_parse embedding vector(1536);
+alter table processed_document_chunks add column content_llama_parse text;
+alter table processed_document_chunks add column embedding_llama_parse vector(1536);
